ec6ab3519e51fa0ca2f87faaf4626da2d5920cb3
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2018-11-16 23:55:10 +0200

    Update VendingMachine.h

--- Assignment 3/VendingMachine.h
+++ Assignment 3/VendingMachine.h
@@ -10,18 +10,18 @@ using namespace std;
 class VendingMachine
 {
 private:
-	Item *items[10];
-	PayAndReminder* Money;
+	Item *items[10];		//arrey of size 10 to Item
+	PayAndReminder* Money; // pointer to PayAndRemider
 
 public:
-	VendingMachine();
-	void Menu();
-	void Check(int c);
-	bool Continue();
-	char * GetItemName(int index);
-	VendingMachine(char ProductName[10][30], double Price[10]);
+	VendingMachine();											//Default Constractor
+	VendingMachine(char ProductName[10][30], double Price[10]);	//Another Constractor
+
+	void Menu();					//Menu function that printing and managing the interface 
+	void Check(int &choice);		//check function, a function that help "Menu"
+	void Continue(int& c);			//Same as Check, a function help the "Menu"
 
 
-	virtual ~VendingMachine();
+	virtual ~VendingMachine(); //Destractor
 };
 
