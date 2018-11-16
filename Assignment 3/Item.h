0c8285501ef5a436ad57307d7737fa0b1b465765
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2018-11-16 23:53:26 +0200

    Update Item.h

--- Assignment 3/Item.h
+++ Assignment 3/Item.h
@@ -7,20 +7,20 @@ using namespace std;
 class Item
 {
 private:
-	char name[20];
-	double price;
-	int NumberOfProducts;
+	char name[20]; //the name of the item
+	double price;			// the price of the item
+	int amount;	// the amount of the item
 
 public:
-	Item();
-	Item(char name[], double price, int number);
-	void SetName(char name[]);
-	void SetPrice(double price);
-	void SetNumberOfProducts(int number);
+	Item();	//Constractor
+	Item(char name[], double price, int amount);		//Constractor
+	void SetName(char name[]);		//Setting the Name
+	void SetPrice(double price);	//Setting the price
+	void SetNumberOfProducts(int number);		//Setting the amount of the item		
 
-	char* GetName();
-	double GetPrice();
-	int GetnumberOfProducts();
-	virtual ~Item();
+	char* GetName();		//Getter for the name
+	double GetPrice();			//Getter for the Price
+	int GetnumberOfProducts();	//Getter for the amount of the item
+	virtual ~Item();		//Destractor
 };
 
