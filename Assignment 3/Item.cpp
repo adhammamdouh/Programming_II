1d6f0586ea11170571f7c877e2f5f8c8d4ffce68
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2018-11-16 23:53:02 +0200

    Update Item.cpp

--- Assignment 3/Item.cpp
+++ Assignment 3/Item.cpp
@@ -2,36 +2,38 @@
 
 
 
-Item::Item()
+Item::Item()		//Default destractor
 {
 }
-Item::Item(char name[], double price, int number) {
-	strcpy_s(this->name, name);
-	this->price = price;
-	this->NumberOfProducts = number;
+Item::Item(char name[], double price, int amount) {			//Destractor
+	strcpy_s(this->name, name);		//copying the "name (parameter)" int the "name(data member)"
+	this->price = price;			//setting the "price(parameter)" to "price(data member)"
+	this->amount = amount;			//setting the "amount(parameter)" to "amount(data member)"
 }
 
-void Item::SetName(char name[]) {
+void Item::SetName(char name[]) {		//Setter for the name
 	strcpy_s(this->name, name);
 }
-void Item::SetPrice(double price) {
+
+void Item::SetPrice(double price) {		//Setter for the Price
 	if (price > 0)
 		this->price = price;
 	else
 		this->price = 0.0;
 }
-void Item::SetNumberOfProducts(int number) {
+
+void Item::SetNumberOfProducts(int number) {	//Setter for the amount of the item
 	if (number >= 0)
-		this->NumberOfProducts = number;
+		this->amount = number;
 	else
-		this->NumberOfProducts = 0;
+		this->amount = 0;
 }
 
-char* Item::GetName() { return name; }
-double Item::GetPrice() { return price; }
-int Item::GetnumberOfProducts() { return NumberOfProducts; }
+char* Item::GetName() { return name; }			//Getter for the name
+double Item::GetPrice() { return price; }		//Getter for the price
+int Item::GetnumberOfProducts() { return amount; }		//Getter for the amount
 
 
-Item::~Item()
+Item::~Item()	//default Destractor
 {
 }
