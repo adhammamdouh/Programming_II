cbc6c89104fb4b5a4d8a53dc1bd6be158bc6409b
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2018-11-17 11:55:07 +0200

    Update PayAndReminder.h

--- Assignment 3/PayAndReminder.h
+++ Assignment 3/PayAndReminder.h
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-enum ReminderProcess	//enum for 
+enum ReminderProcess	//enum for improve the readability of the code in the remining process
 {
-	NOTExist = 0,
+	NOTExist = 0,			
 	okay,
 	NoRmindersExist,
 	LessThanPrice,
@@ -16,20 +16,20 @@ enum ReminderProcess	//enum for
 class PayAndReminder
 {
 private:
-	double money;
-	double price;
-	double RemainingMoney;
-	double MoneyExist[5] ;
-	int MoneyECount[5];
+	double money;			//money that the user entered
+	double price;			//the price of the item the the user choosed
+	double RemainingMoney;	//the remining amount of money that the user need
+	double MoneyExist[5] ;	//an array for the conis in the machine 20, 10, 5, 1, 0.5
+	int MoneyECount[5];		//another array for the amount of the every coin in the machine
 public:
-	PayAndReminder();
-	PayAndReminder(int p, double money);
-	void SetMoney(double Money);
-	void SetPrice(double Price);
-	double GetMoney();
-	double GetRemaingMoney();
-	short CalReminder();
+	PayAndReminder();		//default constractor
+	PayAndReminder(int p, double money);	//Constractor with p, money parameters
+	void SetMoney(double Money);	//setter for the money
+	void SetPrice(double Price);	//setter for the price of the item
+	double GetMoney();				//getter of the money 
+	double GetRemaingMoney();		//getter for the remaingibg amount of money
+	short CalReminder();			//calReminder is function for calculating the remaining amount of money for the user
 
-	virtual ~PayAndReminder();
+	virtual ~PayAndReminder();		//destractor
 };
 
